a23ca3bb2b608a069ea9325c68a231cb35e8a793
Seb <seebeesbt@gmail.com>
2022-07-02 22:22:03 +0100

    e

--- src/main/java/minestat/MinecraftEventListener.java
+++ src/main/java/minestat/MinecraftEventListener.java
@@ -1,32 +1,28 @@
 package main.java.minestat;
 
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class MinecraftEventListener implements Listener {
 
-	private MineStat plugin = MineStat.getPlugin(MineStat.class);
+	private static MineStat plugin = MineStat.getPlugin(MineStat.class);
+	private static final int TOTAL = plugin.getServer().getMaxPlayers();
 	
 	private static int online = 0;
 	
-	@EventHandler(priority = EventPriority.HIGHEST)
+	@EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-		int total = plugin.getServer().getMaxPlayers();
 		online++;
-		plugin.getLogger().info("Player has joined - (" + online + "/" + total + ")");
-		Discord.SetMemberCount(online, total);
+		Discord.SetMemberCount(online, TOTAL);
 		Discord.UpdateMessage();
     }
 	
-	@EventHandler(priority = EventPriority.HIGHEST)
+	@EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
-		int total = plugin.getServer().getMaxPlayers();
 		online--;
-		plugin.getLogger().info("Player has left - (" + online + "/" + total + ")");
-		Discord.SetMemberCount(online, total);
+		Discord.SetMemberCount(online, TOTAL);
 		Discord.UpdateMessage();
     }
 	
